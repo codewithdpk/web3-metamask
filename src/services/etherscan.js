6cc01d45b0f4d5f5a6abaec0e4d98e2557a13707
Deepak Suthar <codewithdeepak@yahoo.com>
2021-11-23 16:59:23 +0530

    Fixed etherscan url

--- src/services/etherscan.js
+++ src/services/etherscan.js
@@ -1,4 +1,4 @@
-const DOMAIN = `http://api-ropsten.etherscan.io/api?apikey=${process.env.REACT_APP_ETHERSCAN_API_TOKEN}&`;
+const DOMAIN = `https://api-ropsten.etherscan.io/api?apikey=${process.env.REACT_APP_ETHERSCAN_API_TOKEN}&`;
 
 export const getAllLogs = (account) => {
   const SUB_DOMAIN = `module=account&action=txlist&address=${account}&startblock=0&endblock=99999999&sort=asc`;
